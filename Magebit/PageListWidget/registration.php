2fdba38c9224bfc1ae16e2227f3e2113c5405e6c
rinards-korsaks <83955923+rinards-korsaks@users.noreply.github.com>
2021-05-18 15:36:27 +0300

    Update registration.php

--- Magebit/PageListWidget/registration.php
+++ Magebit/PageListWidget/registration.php
@@ -1,3 +1,4 @@
+<?php
 /**
 * This file is part of the Magebit PageListWidget package.
 *
@@ -12,7 +13,6 @@
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
 */
-<?php
 
 use Magento\Framework\Component\ComponentRegistrar;
 
